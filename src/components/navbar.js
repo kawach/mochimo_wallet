3344d398a468608cb8367d31c87046c452f3c20a
Easy Peasy / Kawach <victor_timmerman@hotmail.com>
2021-11-11 00:52:56 +0100

    navbar changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -120,16 +120,9 @@ const NavbarStart = () => {
                 <a className="navbar-link">
                     More
                 </a>
-
                 <div className="navbar-dropdown">
                     <a className="navbar-item">
                         About
-                    </a>
-                    <a className="navbar-item">
-                        Jobs
-                    </a>
-                    <a className="navbar-item">
-                        Contact
                     </a>
                     <hr className="navbar-divider"/>
                     <a className="navbar-item">
