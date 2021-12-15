b5099373241ebf1187d59acee82680586aa2cac4
Easy Peasy / Kawach <victor_timmerman@hotmail.com>
2021-12-15 03:50:14 +0100

    fix input

--- src/components/input.js
+++ src/components/input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Input = ({id,label, type, placeholder, onChange, handleBlur, value = ""})=>{
+export const Input = ({id,label, type, placeholder, onChange, handleBlur, value})=>{
     return (
         <div className="field">
             <label className="label">{label}</label>
